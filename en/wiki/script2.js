97b658c2daf1dc3f7e8168f17f7b0c015ba8a647
iakobieli <135122468+iakobieli@users.noreply.github.com>
2025-03-17 09:36:48 +0400

    Add files via upload

---
+++ en/wiki/script2.js
@@ -0,0 +1,69 @@
+function on() {
+  document.getElementById("overlay").style.display = "block";
+
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+
+}
+
+function on2() {
+  document.getElementById("overlay2").style.display = "block";
+
+}
+
+function off2() {
+  document.getElementById("overlay2").style.display = "none";
+
+}
+
+function on3() {
+  document.getElementById("overlay3").style.display = "block";
+
+}
+
+function off3() {
+  document.getElementById("overlay3").style.display = "none";
+
+}
+
+function on4() {
+  document.getElementById("overlay4").style.display = "block";
+
+}
+
+function off4() {
+  document.getElementById("overlay4").style.display = "none";
+
+}
+
+function on5() {
+  document.getElementById("overlay5").style.display = "block";
+
+}
+
+function off5() {
+  document.getElementById("overlay5").style.display = "none";
+
+}
+
+function on6() {
+  document.getElementById("overlay6").style.display = "block";
+
+}
+
+function off6() {
+  document.getElementById("overlay6").style.display = "none";
+
+}
+
+function on7() {
+  document.getElementById("overlay7").style.display = "block";
+
+}
+
+function off7() {
+  document.getElementById("overlay7").style.display = "none";
+
+}
